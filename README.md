1638443d82ba18a5028c53e7df6dd31d60621207
mikeklimanek <mike.klimanek@gmail.com>
2024-02-13 04:05:03 +0100

    visualization of Pi with million digits and usage counter of digits

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PYsualization
 Python visualization projects
+
+* created a visualization for number Pi with million digits, as well as a counter how many times a number 0-9 was used in Pi
+
+![alt text](Pi.PNG)
+
+![alt text](Pi2.PNG)
+
+![alt text](Pi3.PNG)
+
--- project_pi/main.py
+++ project_pi/main.py
@@ -9,7 +9,7 @@ with open("pi-1million.txt", "r") as file:
 digits = [int(digit) for digit in pi]
 
 pygame.init()
-width, height = 1200, 900
+width, height = 2560, 1440
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pi Visualization")
 
@@ -50,6 +50,31 @@ while running:
     current_digit = digits[index]
     index += 1
     counts[current_digit] += 1
+    
+    for i in range(len(digits) - 1):
+        start_digit = digits[i]
+        end_digit = digits[i + 1]
+
+        angle_start = (start_digit * (2 * math.pi) / len(counts)) + rotation_angle_radians
+        angle_end = (end_digit * (2 * math.pi) / len(counts)) + rotation_angle_radians
+
+
+
+
+        x_start = width // 2 + 200 * scaling_factor * math.cos(angle_start)
+        y_start = height // 2 + 200 * scaling_factor * math.sin(angle_start)
+        x_end = width // 2 + 200 * scaling_factor * math.cos(angle_end)
+        y_end = height // 2 + 200 * scaling_factor * math.sin(angle_end)
+        rotation_angle_radians += 1
+        
+
+        line_color = pygame.Color(*colors[color_index % len(colors)])
+        color_index += 1
+
+        pygame.draw.line(screen, line_color, (x_start, y_start), (x_end, y_end), 1)
+        pygame.display.flip()
+        pygame.time.delay(1)
+        clock.tick(100)
 
 
 
