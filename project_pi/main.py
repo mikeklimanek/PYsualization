9e24aaa3ed1c2f6b24185e29ec0624d3c64c8030
mikeklimanek <mike.klimanek@gmail.com>
2024-02-13 03:53:04 +0100

    creating a visualization of Pi

--- project_pi/main.py
+++ project_pi/main.py
@@ -24,3 +24,35 @@ clock = pygame.time.Clock()
 counts = [0] * 10
 index = 0
 positions = {}
+
+scaling_factor = 3
+
+
+font = pygame.font.Font(None, 36)
+
+def get_offset_angle(angle, offset):
+    return angle + offset
+
+def get_offset_coordinates(x, y, offset, angle):
+    new_x = x + offset * scaling_factor * math.cos(angle)
+    new_y = y + offset * scaling_factor * math.sin(angle)
+    return new_x, new_y
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    rotation_angle_radians = math.radians(90)
+    pygame.draw.circle(screen, (255, 255, 255), (width // 2, height // 2), 605, 5)
+
+    current_digit = digits[index]
+    index += 1
+    counts[current_digit] += 1
+
+
+
+
+
+    pygame.quit()
